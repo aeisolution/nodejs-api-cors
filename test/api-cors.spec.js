d6f59c36014d6f7e1ea2d4446539bf302a78ae60
aeisolution <info@aeisolution.it>
2015-07-21 22:16:29 +0200

    fake test

--- test/api-cors.spec.js
+++ test/api-cors.spec.js
@@ -1,31 +1,81 @@
-var chai = require('chai'),
-		expect = chai.expect,
-		cors = require('../lib/cors');
+(function() {
+	'use strict';
 
-describe('api CORS', function() {
-	it('will return error.args if it has less than 3 params', function() {
-		var retVal = ['error.args'];
-		
-		expect(cors()).to.be.deep.equal(retVal);
-		expect(cors('one')).to.be.deep.equal(retVal);
-		expect(cors('one', 'two')).to.be.deep.equal(retVal);		
-		expect(cors('one', 'two', 'three')).to.not.be.equal(retVal);		
-	});
+	var chai = require('chai'),
+			expect = chai.expect,
+			cors = require('../lib/cors');
+ 	
+ 	
+ 	var fakeRequest = function(headers) {
+		return {
+			headers: headers || {
+				'origin': 'request.com',
+				'access-control.request-headers': 'requestedHeader1, requestedHeader2'
+			},
+			pause: function() {
+				return;
+			},
+			resume: function() {
+				return;
+			}
+		};
+	},
+	fakeResponse = function() {
+		var headers = {};
+		return {
+			allHeaders: function() {
+				return headers;
+			},
+			getHeader: function(key) {
+				return headers[key];
+			},
+			set: function(key, val) {
+				headers[key] = val;
+			},
+			get: function(key) {
+				return headers[key];
+			}
+		};
+	};
+
 	
-	it('will return error.params the 3 params are not object', function() {
-		var retVal = ['error.inputs'];
-		
-		expect(cors('one', 	'two', 	'three')).to.be.deep.equal(retVal);
-		expect(cors({}, 		'two', 	'three')).to.be.deep.equal(retVal);
-		expect(cors('one', 	{}, 		'three')).to.be.deep.equal(retVal);
-		expect(cors('one', 	'two', 	{})).to.be.deep.equal(retVal);
-		
-		expect(cors({}, 		{}, 		'three')).to.be.deep.equal(retVal);
-		expect(cors({}, 		'two',	{})).to.be.deep.equal(retVal);
-		expect(cors('one', 	{}, 		{})).to.be.deep.equal(retVal);
-		expect(cors({}, 	{}, 		{})).to.be.deep.equal(retVal);
-		
-		expect(cors({}, 	{}, 		function(){ return true; })).to.not.be.equal(retVal);		
-	});
+	
+	describe('api CORS', function() {
+		it('will return error.args if it has less than 3 params', function() {
+			var retVal = ['error.args'];
 
-});
+			var req, res, next;
+			req = fakeRequest();
+			res = fakeResponse();
+			
+			expect(cors()()).to.be.deep.equal(retVal);
+			expect(cors()('one')).to.be.deep.equal(retVal);
+			expect(cors()('one', 'two')).to.be.deep.equal(retVal);		
+			expect(cors()('one', 'two', 'three')).to.not.be.equal(retVal);		
+		});
+
+		it('will return error.params the 3 params are not object', function(done) {
+			var retVal = ['error.inputs'];
+
+			var req, res, next;
+			req = fakeRequest();
+			res = fakeResponse();
+			
+			expect(cors()('one', 	'two', 	'three')).to.be.deep.equal(retVal);
+			expect(cors()({}, 		'two', 	'three')).to.be.deep.equal(retVal);
+			expect(cors()('one', 	{}, 		'three')).to.be.deep.equal(retVal);
+			expect(cors()('one', 	'two', 	{})).to.be.deep.equal(retVal);
+
+			expect(cors()({}, 		{}, 		'three')).to.be.deep.equal(retVal);
+			expect(cors()({}, 		'two',	{})).to.be.deep.equal(retVal);
+			expect(cors()('one', 	{}, 		{})).to.be.deep.equal(retVal);
+			expect(cors()({}, 	{}, 		{})).to.be.deep.equal(retVal);
+
+			next = function() { 
+				done(); 
+			};
+			cors()(req, res, next);		
+		});
+
+	});
+}());
